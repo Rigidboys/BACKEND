65dd280afed89c661135df1aa0eb47e5dcc1b18b
상현 <91441630+csh6779@users.noreply.github.com>
2025-06-02 17:05:02 +0900

    뉴 리지드보이즈 고객관리
    고객관리입니다.

---
+++ rigidboys 고객사관리/rigidboys 고객사관리/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace rigidboys_고객사관리
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
